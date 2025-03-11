fe64117597d77d863bf711291fdaa77a3c5f37a9
Prantik Sanki <prantiksanki@gmail.com>
2025-03-11 23:44:11 +0530

    Adding react hook form

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -5,25 +5,41 @@ function Select({
     label,
     className,
     ...props
-}, ref) {
+}, ref) 
+{
     const id = useId()
   return (
     <div className='w-full'>
+
+        
         {label && <label htmlFor={id} className=''></label>}
+
+        
         <select
         {...props}
         id={id}
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         >
-            {options?.map((option) => (
+            
+        {options?.map((option) =>     // OPTIONAL LOOP
+        (
                 <option key={option} value={option}>
                     {option}
                 </option>
-            ))}
+        )
+        )}
         </select>
+
+
+        
     </div>
   )
 }
 
 export default React.forwardRef(Select)
+
+// FORWARD REF :-
+// ✅ When a parent needs direct access to a child's DOM element.
+// ✅ When creating reusable input fields, buttons, or modals that require external control.
+// ✅ When integrating third-party libraries that rely on refs.
